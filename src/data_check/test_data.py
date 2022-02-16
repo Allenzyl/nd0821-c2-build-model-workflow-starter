814c964406eb615aff5c35d827727f80fe25989c
Yilun Zhang <yilun.zhang@rhsmith.umd.edu>
2022-02-16 00:11:41 -0500

    project finished

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -4,7 +4,6 @@ import scipy.stats
 
 
 def test_column_names(data):
-
     expected_colums = [
         "id",
         "name",
@@ -63,3 +62,9 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    assert np.sum(~data['price'].between(min_price,max_price)) == 0
+
